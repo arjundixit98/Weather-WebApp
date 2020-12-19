660d3816edce1212abb2825e2f4ab1e2d47f3a5b
Arjun <arjun.dixit98@gmail.com>
2020-12-20 03:38:47 +0530

    Weather App first commit

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const fs = require('fs');
+const requests = require('requests');
+
+
+const homeFile = fs.readFileSync("./files/home.html", "utf-8");
+
+const replaceVal = (tempVal, orgVal) => {
+    let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
+    temperature = temperature.replace("{%country%}", orgVal.sys.country);
+    temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+    temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+    temperature = temperature.replace("{%location%}", orgVal.name);
+    temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
+
+    return temperature;
+
+
+}
+const server = http.createServer((req,res) => {
+
+    if(req.url == "/")
+    {
+        requests("http://api.openweathermap.org/data/2.5/weather?q=Etah&units=metric&appid=383990250f2b2488fd5f41d0b8d098da")
+
+        .on('data', (chunk) => {
+        const objData = JSON.parse(chunk);    
+        const arrData = [objData];
+        //console.log(arrData[0].main.temp);
+        const realTimeData = arrData.map((val) => replaceVal(homeFile, val)).join("");
+        res.write(realTimeData);
+       // console.log(realTimeData);
+        })
+
+        
+
+        .on('end', (err) => {
+        if (err) return console.log('connection closed due to errors', err);
+        res.end();
+        });
+    }
+    else{
+        res.end("File not found");
+    }
+    
+});
+
+server.listen(8000, "127.0.0.1");
